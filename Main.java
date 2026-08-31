f3e70d551125424704e9170f6e141244d1fba252
codeharshit-ty <hhsingh2011@gmail.com>
2026-08-31 21:48:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int febDays = 29;
+        String result;
+
+        result = (febDays == 29) ? "Leap Year" : "Not a Leap Year";
+
+        System.out.println(result);
+    }
+}
